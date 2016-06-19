bb26a849126fba8a5238f4a1a4fdfae5e4f9e23d
Roberto <robertooliveiraestudo@gmail.com>
2016-06-19 18:22:34 -0300

    Fuck the system

---
+++ SegundoBI/Partial/Proprietario.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SegundoBI
+{
+    partial class Proprietario
+    {
+        public void incluir()
+        {
+            Sistema_ImoveisEntities oDB = new Sistema_ImoveisEntities();
+            oDB.Proprietario.add(this);
+            oDB.SaveChanges();
+            oDB.Dispose();
+        }
+
+        public void alterar()
+        {
+            using (Sistema_ImoveisEntities oDB = new Sistema_ImoveisEntities())
+            {
+                oDB.Entry(this).State = System.Data.Entity.EntityState
+            }
+        }
+    }
+}
