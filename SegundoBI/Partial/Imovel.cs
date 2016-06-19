1273ac366df13d474a92588d55294a4cc9f016e3
Gustavo <gustavosabonare@hotmail.com>
2016-06-19 17:46:28 -0300

    Partial

---
+++ SegundoBI/Partial/Imovel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SegundoBI
+{
+    public partial class Imovel
+    {
+        public void Incluir()
+        {
+            Sistema_ImoveisEntities oDB = new Sistema_ImoveisEntities();
+            oDB.Dispose();
+        }
+    }
+}
